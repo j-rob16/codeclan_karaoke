70dc9b684e40cd0aae0e0b4ac228540bc8284862
James R <jimmer1609@gmail.com>
2022-05-30 09:53:08 +0900

    Can check in/out guests from the rooms

--- classes/room.py
+++ classes/room.py
@@ -4,3 +4,13 @@ class Room:
         self.room_number = room_number
         self.capacity = capacity
         self.playlist = playlist
+        self.occupants = []
+
+    def check_in_guest(self, guest):
+        self.occupants.append(guest)
+
+    def check_out_guest(self, guest_name):
+        for guest in self.occupants:
+            if guest.name == guest_name:
+                self.occupants.remove(guest)
+
--- tests/test_room.py
+++ tests/test_room.py
@@ -1,12 +1,15 @@
 import unittest
 
+from classes.guest import Guest
 from classes.room import Room
 from classes.song import Song
+
 
 class TestRoom(unittest.TestCase):
 
     def setUp(self):
         self.room_1 = Room(1, 10, [Song('Billie Jean', 'Michael Jackson')])
+        self.guest_1 = Guest('James', 28, 'Billie Jean')
 
     def test_room_has_number(self):
         self.assertEqual(1, self.room_1.room_number)
@@ -16,3 +19,12 @@ class TestRoom(unittest.TestCase):
 
     def test_room_has_playlist(self):
         self.assertEqual('Billie Jean', self.room_1.playlist[0].title)
+
+    def test_can_check_in_guest(self):
+        self.room_1.check_in_guest(self.guest_1)
+        self.assertEqual(self.guest_1, self.room_1.occupants[0])
+
+    def test_can_check_out_guest(self):
+        self.room_1.check_in_guest(self.guest_1)
+        self.room_1.check_out_guest(self.guest_1.name)
+        self.assertEqual(0, len(self.room_1.occupants))
